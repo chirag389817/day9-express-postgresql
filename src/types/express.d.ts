c8fa2906d0fd0027600c8be4be37952b34d2c06a
chirag389817 <chirag389817@gmail.com>
2025-03-08 16:20:01 +0530

    fix: dev script to include files in ts-node

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import { UserInterface } from "./user";
 declare global {
     namespace Express {
         interface Request {
-            user?: UserInterface; // Add user property, assuming User is your user model or type
+            user?: UserInterface;
         }
     }
 }
